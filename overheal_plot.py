3c59bfe67ddc0d2e6da40a91a46e8d18f05c685e
Filip Gokstorp <filip@gokstorp.se>
2020-04-30 12:23:41 +0100

    Updates overheal plot. Now shows fraction of casts that have overheal. Can also make separate plots for different spells.

--- overheal_plot.py
+++ overheal_plot.py
@@ -11,9 +11,10 @@ from textwrap import wrap
 
 import overheal_table as ot
 import process_raw_logs as raw
+from spell_data import SPELL_NAMES
 
 
-def process_log(player_name, log_file, ignore_crit=False, spell_id=None, **kwargs):
+def process_log(player_name, log_file, spell_power=500, ignore_crit=False, spell_id=None, **kwargs):
     heal_lines, periodic_lines = raw.get_lines(player_name, log_file)
 
     os.makedirs("figs", exist_ok=True)
@@ -21,31 +22,48 @@ def process_log(player_name, log_file, ignore_crit=False, spell_id=None, **kwarg
     # Group lines
     names, heal_lines = ot.group_processed_lines(heal_lines, ignore_crit, spell_id=spell_id)
 
-    spell_powers = np.arange(0, 500 + 1, 5)
+    spell_powers = np.linspace(0, spell_power, 101)
+    sp = max(spell_powers) - spell_powers
     total_heals = []
     total_overheals = []
     total_actual_heals = []
 
-    for sp in spell_powers:
-        total_data, _ = ot.aggregate_lines(heal_lines, spell_power=sp)
+    nn_underheals = []
+    nn_overheals = []
+    nn_half_overheals = []
+    nn_full_overheals = []
+
+    for s in spell_powers:
+        total_data, _ = ot.aggregate_lines(heal_lines, spell_power=s)
 
         n_heals = total_data[0]
+        n_overheals = total_data[1]
+        n_half_overheals = total_data[2]
+        n_full_overheals = total_data[3]
+
         heal = total_data[4]
         overheal = total_data[5]
         actual_heal = total_data[4] - total_data[5]
+
+        # print(f"sp: {s}  N: {n_heals}  O: {n_overheals}  H: {heal:.0f}  OH: {overheal:.0f}")
 
         total_heals.append(heal / n_heals)
         total_overheals.append(overheal / n_heals)
         total_actual_heals.append(actual_heal / n_heals)
 
-    plt.figure()
-    plt.plot(spell_powers, total_heals, label="Total healing")
-    plt.plot(spell_powers, total_overheals, label="Total overheal")
-    plt.plot(spell_powers, total_actual_heals, label="Total actual heal")
+        nn_underheals.append((n_heals - n_overheals) / n_heals)
+        nn_overheals.append(n_overheals / n_heals)
+        nn_half_overheals.append(n_half_overheals / n_heals)
+        nn_full_overheals.append(n_full_overheals / n_heals)
 
-    plt.title("\n".join(wrap("Average healing per cast as you artifically remove spell power.", 60)))
-    plt.xlabel("Removed heal power")
-    plt.ylabel("Healing")
+    plt.figure()
+    plt.plot(sp, total_heals, label="Total heal")
+    plt.plot(sp, total_overheals, label="Overheal")
+    plt.plot(sp, total_actual_heals, label="Actual heal")
+
+    plt.title("\n".join(wrap("Average healing per cast as +heal varies", 60)))
+    plt.xlabel("Heal power")
+    plt.ylabel("Healing output")
     plt.ylim([0, None])
 
     plt.grid()
@@ -54,50 +72,60 @@ def process_log(player_name, log_file, ignore_crit=False, spell_id=None, **kwarg
 
     plt.savefig("figs/total_heal.png")
 
+    # number of heals figure
+    fig = plt.figure(figsize=(10, 4), constrained_layout=True)
+    ax1, ax2 = fig.subplots(1, 2)
+
+    ax1.plot(sp, nn_underheals, label="Underheal")
+    ax1.plot(sp, nn_overheals, label="Any overheal")
+    ax1.plot(sp, nn_half_overheals, label="Half overheal")
+    ax1.plot(sp, nn_full_overheals, label="Full overheal")
+
+    title = "Number of overheals as a +heal varies"
+    if spell_id:
+        title = SPELL_NAMES[spell_id] + "\n" + title
+
+    ax1.set_title(title)
+    ax1.set_xlabel("Heal power")
+    ax1.set_ylabel("Overheal fraction of total casts")
+    ax1.set_ylim([0.0, 1.0])
+
+    ax1.grid()
+    ax1.legend()
+
     # gradient figure
-    d_sp = np.diff(spell_powers)
-    sp = 0.5 * (spell_powers[1:] + spell_powers[:-1])
-
-    plt.figure()
-    plt.plot(sp, np.diff(total_heals) / d_sp, label="Total healing")
-    plt.plot(sp, np.diff(total_overheals) / d_sp, label="Total overhealing")
-    plt.plot(sp, np.diff(total_actual_heals) / d_sp, label="Total actual heal")
-
-    plt.title("\n".join(wrap("Average healing per spell power lost as you artifically remove spell power.", 70)))
-    plt.xlabel("Removed heal power")
-    plt.ylabel("d(Healing)/d(Spell power)")
-    # plt.ylim([0, None])
-
-    plt.grid()
-    plt.legend()
-    plt.tight_layout()
-
-    # flipped figure
-    sp = max(spell_powers) - spell_powers
     d_sp = np.diff(sp)
     sp = 0.5 * (sp[1:] + sp[:-1])
 
     dh = np.diff(total_actual_heals) / d_sp
 
-    plt.figure()
-    plt.plot(sp, np.diff(total_heals) / d_sp, label="Total healing")
-    plt.plot(sp, np.diff(total_overheals) / d_sp, label="Total overhealing")
-    plt.plot(sp, dh, label="Total actual heal")
+    ax2.plot(sp, np.diff(total_heals) / d_sp, label="Total heal")
+    ax2.plot(sp, np.diff(total_overheals) / d_sp, label="Overheal")
+    ax2.plot(sp, dh, label="Actual heal")
 
     # linear fit of coefficient
-    coef = np.polyfit(sp, dh, 1)
-    plt.plot(sp, np.poly1d(coef)(sp), "k--", label=f"Linear fit, Sc={coef[0] * 100:.2g} * 100h + {coef[1]:.2g}")
+    # coef = np.polyfit(sp, dh, 1)
+    # ax2.plot(sp, np.poly1d(coef)(sp), "k--", label=f"Linear fit, Sc={coef[0] * 100:.2g} * 100h + {coef[1]:.2g}")
 
-    plt.title("\n".join(wrap("Starting from already reduced spell power, average healing per spell power gained as you recover spell power. This is the plot you could maybe extrapolate to infer value of additional spell power. Note that the blue line is the average spell coefficient of the casting profile.", 70)))
-    plt.xlabel("Recovered heal power")
-    plt.ylabel("d(Healing)/d(Spell power)")
-    plt.ylim([0, None])
+    # plt.title("\n".join(wrap("Starting from already reduced spell power, average healing per spell power gained as you recover spell power. This is the plot you could maybe extrapolate to infer value of additional spell power. Note that the blue line is the average spell coefficient of the casting profile.", 70)))
 
-    plt.grid()
-    plt.legend()
-    plt.tight_layout()
+    title = "Effective spell coefficient as total +heal varies"
+    if spell_id:
+        title = SPELL_NAMES[spell_id] + "\n" + title
 
-    plt.savefig("figs/adj_spell_coef.png")
+    ax2.set_title(title)
+    ax2.set_xlabel("Heal power")
+    ax2.set_ylabel("d(Healing)/d(Spell power)")
+    ax2.set_ylim([0.0, 1.0])
+
+    ax2.grid()
+    ax2.legend()
+
+    fig_name = "overheal"
+    if spell_id:
+        fig_name += "_" + spell_id
+
+    plt.savefig(f"figs/{fig_name}.png")
 
     plt.show()
 
@@ -122,8 +150,9 @@ Header explaination:
     parser.add_argument("player_name", help="Player name to analyse overheal for")
     parser.add_argument("log_file", help="Path to the log file to analyse")
     parser.add_argument("--ignore_crit", action="store_true", help="Remove critical heals from analysis")
-    parser.add_argument("--spell_id", type=str, help="Spell id to filter for")
+    parser.add_argument("--spell_id", type = str, help = "Spell id to filter for")
+    parser.add_argument("-p", "--spell_power", type=int, help="Spell power range to look at", default=500)
 
     args = parser.parse_args()
 
-    process_log(args.player_name, args.log_file, ignore_crit=args.ignore_crit, spell_id=args.spell_id)
+    process_log(args.player_name, args.log_file, args.spell_power, ignore_crit=args.ignore_crit, spell_id=args.spell_id)
