2a1bd3c1e5ca3bac48f19f60ff26b66c97b20fbf
Filip Gokstorp <filip@gokstorp.se>
2020-05-22 23:30:04 +0100

    Updates read_from_api. Now reads in all data with multiple requests.

--- read_from_api.py
+++ read_from_api.py
@@ -1,5 +1,6 @@
 """Read data from WCL API."""
 import requests
+from json.decoder import JSONDecodeError
 from datetime import datetime
 
 try:
@@ -11,9 +12,33 @@ except FileNotFoundError:
 API_ROOT = "https://classic.warcraftlogs.com:443/v1"
 
 
+def _get_api_request(url):
+    """
+    Get an API request and check status code.
+
+    :param url: full url with request details to use.
+    """
+    req = requests.get(url)
+
+    if not req.status_code == 200:
+        print("Error getting an API request:")
+        print(req.text)
+        exit(200)
+
+    data = None
+    try:
+        data = req.json()
+    except JSONDecodeError:
+        print("WarcraftLogs did not return proper JSON, it is likely down for maintenance.")
+        print("Request response:", req.text)
+        exit(300)
+
+    return data
+
+
 def get_fights(code):
     url = f"{API_ROOT}/report/fights/{code}?api_key={API_KEY}"
-    return requests.get(url).json()
+    return _get_api_request(url)
 
 
 def get_player_names(code):
@@ -30,17 +55,11 @@ def get_player_names(code):
     return player_names
 
 
-def get_heals(code, start=0, end=None, names=None):
+def _get_heals(code, start=0, end=None, names=None, for_player=None):
     """Gets all heals for the log"""
 
     if end is None:
         end = start + 3 * 60 * 60 * 1000  # look at up to 3h of data
-
-    url = f"{API_ROOT}/report/events/healing/{code}?start={start}&end={end}&api_key={API_KEY}"
-
-    req = requests.get(url)
-    data = req.json()
-    events = data["events"]
 
     if names is None:
         names = dict()
@@ -49,67 +68,129 @@ def get_heals(code, start=0, end=None, names=None):
     periodics = []
     absorbs = []
 
-    for e in events:
-        try:
-            timestamp = e["timestamp"]
-            timestamp = datetime.fromtimestamp(timestamp / 1000).time()
-            spell_id = e["ability"]["guid"]
+    print("Fetching data from WCL...")
+    progress_bar_length = 70
 
-            source = e["sourceID"]
-            source = names.get(source, f"[pid {source}]")
-            target = e["targetID"]
-            target = names.get(target, f"[pid {source}]")
-            # event_type = e["type"]
+    # will have to loop to get results
+    request_more = True
+    while request_more:
+        url = f"{API_ROOT}/report/events/healing/{code}?start={start}&end={end}&api_key={API_KEY}"
+        # print(url)
+        progress = start / end
+        n_bars = int(progress * progress_bar_length)
+        bars = "=" * n_bars
+        if n_bars < progress_bar_length:
+            bars += ">"
 
-            amount = int(e["amount"])
+        print(f"[{bars:70s}]  {progress:4.0%}  {start:8d} / {end:8d}", end="\r")
 
-            if e["type"] == "absorb":
-                # Shield absorb
-                absorbs.append((timestamp, source, spell_id, target, amount))
-                continue
+        data = _get_api_request(url)
+        events = data["events"]
+        if "nextPageTimestamp" in data:
+            start = data["nextPageTimestamp"]
+        else:
+            request_more = False
 
-            overheal = int(e.get("overheal", "0"))
+        for e in events:
+            try:
+                timestamp = e["timestamp"]
+                # timestamp = datetime.fromtimestamp(timestamp / 1000.0).time()
+                spell_id = str(e["ability"]["guid"])
 
-            if e.get("tick") == "True":
-                # Periodic tick
-                periodics.append(
-                    (timestamp, source, spell_id, target, amount + overheal, overheal)
+                if "sourceID" not in e:
+                    # heal not from a player, skipping
+                    continue
+
+                source = e["sourceID"]
+                source = names.get(source, f"[pid {source}]")
+
+                if for_player and source != for_player:
+                    continue
+
+                target = e["targetID"]
+                target = names.get(target, f"[pid {source}]")
+                # event_type = e["type"]
+
+                amount = e["amount"]
+
+                if e["type"] == "absorbed":
+                    # Shield absorb
+                    absorbs.append((timestamp, source, spell_id, target, amount, 0, False))
+                    continue
+
+                overheal = e.get("overheal", 0)
+
+                if e.get("tick"):
+                    # Periodic tick
+                    periodics.append(
+                        (timestamp, source, spell_id, target, amount + overheal, overheal, False)
+                    )
+                    continue
+
+                is_crit = e.get("hitType", 1) == 2
+
+                heals.append(
+                    (
+                        timestamp,
+                        source,
+                        spell_id,
+                        target,
+                        amount + overheal,
+                        overheal,
+                        is_crit,
+                    )
                 )
+            except Exception as ex:
+                print(e)
+                print(ex)
 
-            is_crit = e.get("hitType", "1") == "2"
-
-            heals.append(
-                (
-                    timestamp,
-                    source,
-                    spell_id,
-                    target,
-                    amount + overheal,
-                    overheal,
-                    is_crit,
-                )
-            )
-        except:
-            print(e)
+    print(f"[{'=' * progress_bar_length:70s}]  {1.0:4.0%}  {end:8d} / {end:8d}")
+    # print("")
 
     return heals, periodics, absorbs
 
 
+def get_heals(code, start=None, end=None, name=None):
+    """
+    Gets heal events for specified log code.
+
+    :param code: the WarcraftLogs code.
+    :param start: the start time, in milliseconds, to get heals for.
+    :param end: the end time, in milliseconds, to get heals for. If None, gets logs for up to 3 hours.
+    :param name: Optional. Name to filter heal events for.
+
+    :returns (heals, periodic_heals, absorbs), lists of heal events
+    """
+    names = get_player_names(code)
+
+    if start is None:
+        start = 0
+
+    if end is None:
+        fight_data = get_fights(code)
+        end = fight_data["end"] - fight_data["start"]
+
+    return _get_heals(code, start, end, names, for_player=name)
+
+
 if __name__ == "__main__":
     import argparse
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("code", nargs="?", default="ar9nG3bvDpw6HTdq")
+
+    parser.add_argument("code")
+    parser.add_argument("-n", "--name")
+
     args = parser.parse_args()
+    heals, periodics, absorbs = get_heals(args.code, name=args.name)
 
-    code = args.code
+    print("Heals")
+    for h in heals[:20]:
+        print("  ", *h)
 
-    player_names = get_player_names(code)
+    print("Periodics")
+    for h in periodics[:20]:
+        print("  ", *h)
 
-    start = 1 * 60 * 60 * 1000
-    start = 0
-    end = start + 2 * 60 * 1000
-    heals, periodics, absorbs = get_heals(code, start, end, names=player_names)
-
-    for e in heals[:50]:
-        print(*e)
+    print("Absorbs")
+    for h in absorbs[:20]:
+        print("  ", *h)
